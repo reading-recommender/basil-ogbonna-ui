bde40abd034e921a2518170a1203bc57ce0209ad
basilcea <ogbonna.basil3@gmmail.com>
2019-05-24 09:38:09 +0100

    fix bugs - add content to the marketing page - add more about info to about us page

--- public/components/Books/books.js
+++ public/components/Books/books.js
@@ -1,3 +1,17 @@
-class  Books {
-    constructor(book)
+class Books {
+  constructor(book, url) {
+    this.book = book;
+    this.url = url;
+    this.buttonClick();
+    this.read = document.querySelector('button');
+    this.read.addEventListener('click', () => {
+      this.buttonClick();
+    });
+  }
+
+  buttonClick() {
+    window.location.href = this.url;
+  }
 }
+const books = document.querySelectorAll('book');
+books.forEach(book => new Books(book));
