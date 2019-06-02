e018983b80cec5b038214ed8725d68541bd7b0af
basilcea <ogbonna.basil3@gmmail.com>
2019-06-02 22:56:16 +0100

    fix tranperent background color of hamburger

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -8,8 +8,11 @@ document.getElementById('hamburger').onclick = () => {
   });
   if (document.querySelector('.svg-b').classList.contains('fa-book')) {
     document.querySelector('.svg-b').className = 'fas fa-book-open svg-b';
+    document.querySelector('.navbar').className += 'aos-init aos-animate';
+    document.querySelector('nav').setAttribute('data-aos', 'fade-out');
   } else {
     menu.forEach(navlink => navlink.classList.remove('navlink-open'));
     document.querySelector('.svg-b').className = 'fas fa-book svg-b';
+    document.querySelector('nav').removeAttribute('data-aos', 'fade-out');
   }
 };
